6f0ed316cab316c42bd8ac3deb586df65d2f2c18
Michael Anderson <mda2894@gmail.com>
2023-08-11 17:40:20 -0400

    Playing with distance calculations

--- scripts/main.R
+++ scripts/main.R
@@ -14,10 +14,12 @@ conflicts_prefer(
 
 # Data --------------------------------------------------------------------
 
-tarc_feed <- "http://googletransit.ridetarc.org/feed/google_transit.zip"
 tarc_file <- here("data", "tarc_gtfs.zip")
 
-download.file(tarc_feed, destfile = tarc_file)
+# # uncomment this block to download new TARC GTFS file
+# dir.create(here("data"))
+# tarc_feed <- "http://googletransit.ridetarc.org/feed/google_transit.zip"
+# download.file(tarc_feed, destfile = tarc_file)
 
 tarc_gtfs <- read_gtfs(tarc_file)
 # summary(tarc_gtfs)
@@ -32,15 +34,16 @@ graph_data <- tarc_gtfs$stop_times %>%
          stop_sequence, shape_dist_traveled, stop_id, stop_name, arrival_time,
          stop_lat, stop_lon) %>%
   replace_na(list(shape_dist_traveled = 0)) %>%
-  mutate(node_id = paste(trip_id, stop_sequence, sep = "-"))
+  mutate(node_id = paste(trip_id, stop_sequence, sep = "-")) %>%
+  arrange(arrival_time)
 
 # Trip Graph --------------------------------------------------------------
 
-trip_graph_nodes <- graph_data %>%
+# node table for all bus stops
+bus_stops <- graph_data %>%
   mutate(node_type = "bus_stop") %>%
-  select(name = node_id, node_type, route_id, trip_id, direction_id, stop_sequence,
-         stop_id, arrival_time, stop_lat, stop_lon) %>%
-  arrange(trip_id, stop_sequence)
+  select(name = node_id, node_type, route_id, trip_id, direction_id,
+         stop_sequence, stop_id, arrival_time, stop_lat, stop_lon)
 
 # route/trip edges (no transfers)
 trip_graph_edges <- graph_data %>%
@@ -54,18 +57,14 @@ trip_graph_edges <- graph_data %>%
   ungroup() %>%
   mutate(edge_type = "bus_trip") %>%
   select(from, to, edge_type, weight, distance, route_id, trip_id, direction_id,
-         trip_leg = stop_sequence) %>%
-  arrange(trip_id, trip_leg)
+         trip_leg = stop_sequence)
 
-# "rooted forest" with each trip a separate tree
-trip_graph <- tbl_graph(trip_graph_nodes, trip_graph_edges, directed = T)
+trip_graph <- tbl_graph(bus_stops, trip_graph_edges, directed = T)
 # print(trip_graph)
 
 # Add (Same-Stop) Transfers -----------------------------------------------
 
-transfer_graph_nodes <- trip_graph_nodes %>%
-  arrange(stop_id, arrival_time)
-
+# Add "same-stop" transfer edges (i.e. waiting for the next bus)
 transfer_graph_edges <- graph_data %>%
   group_by(stop_id) %>%
   arrange(arrival_time) %>%
@@ -76,15 +75,17 @@ transfer_graph_edges <- graph_data %>%
   filter(row_number() <= n() - 1) %>%
   ungroup() %>%
   mutate(edge_type = "transfer") %>%
-  arrange(stop_id, arrival_time) %>%
   select(from, to, edge_type, weight, distance)
 
-transfer_graph <- tbl_graph(transfer_graph_nodes, transfer_graph_edges,
+transfer_graph <- tbl_graph(bus_stops, transfer_graph_edges,
                             directed = T)
 # print(transfer_graph)
 
+# combine trips w/ transfers
 bus_only_graph <- graph_join(trip_graph, transfer_graph)
 # print(bus_only_graph)
+
+rm(trip_graph, trip_graph_edges, transfer_graph, transfer_graph_edges)
 
 # Bus-Only (Forrest Gump) Routing -----------------------------------------
 
@@ -92,6 +93,7 @@ bus_only_graph <- graph_join(trip_graph, transfer_graph)
 #   filter(grepl("Mall", stop_name)) %>%
 #   View()
 
+# playing with routing between two random bus stops
 start_nodes <- bus_only_graph %>%
   activate("nodes") %>%
   as_tibble() %>%
@@ -106,14 +108,17 @@ target_nodes <- bus_only_graph %>%
   arrange(arrival_time) %>%
   pull(name)
 
+# find shortest paths between every possible connection between the two stops
 dist_matrix <- distances(bus_only_graph, v = start_nodes, to = target_nodes,
                          mode = "out")
 min(dist_matrix)
 ind <- arrayInd(which.min(dist_matrix), dim(dist_matrix))
 
+# grab the two nodes that represent the starting and finishing stops
 best_start <- rownames(dist_matrix)[ind[,1]]
 best_finish <- colnames(dist_matrix)[ind[,2]]
 
+# get the full path between the two
 res <- bus_only_graph %>%
   shortest_paths(from = best_start, to = best_finish,
                  mode = "out", output = "both")
@@ -124,6 +129,20 @@ node_path <- graph_data %>%
   filter(node_id %in% node_list) %>%
   arrange(arrival_time)
 
-# Walking Graph? ----------------------------------------------------------
+# playing with the distances function for potential graph pruning
+start_node <- graph_data %>%
+  arrange(arrival_time) %>%
+  filter(row_number() == 1) %>%
+  pull(node_id)
 
-stops <- tarc_gtfs$stops
+all_dist <- distances(bus_only_graph, v = start_node, mode = "out")
+sum(all_dist != Inf)
+
+# Walking Graph -----------------------------------------------------------
+
+# Look into sfnetworks (would probably require some rewrites)
+# OSMExtract / OSMData for walking data
+# Tidygraph node_distance_from
+
+# Add Library Nodes -------------------------------------------------------
+
