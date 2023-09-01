843cd177261a0e4737d3f77ae52446471a362f13
Michael Anderson <mda2894@gmail.com>
2023-09-01 10:34:54 -0400

    Theoretical minimum

---
+++ scripts/theoretical_minimum.R
@@ -0,0 +1,69 @@
+# Theoretical Minimum
+
+# Setup -------------------------------------------------------------------
+
+library(here)
+library(tidyverse)
+library(TSP)
+
+# Data --------------------------------------------------------------------
+
+load(here("data", "otp_data.RData"))
+
+lib_names <- unique(otp_data$from)
+
+# Lower Bound (walking) ---------------------------------------------------
+
+min_dist_matrix <- matrix(NA, 17, 17)
+
+for (i in 1:17) {
+  for (j in 1:17) {
+    min_dist_matrix[i, j] <- otp_data %>%
+      filter(from == lib_names[i],
+             to == lib_names[j]) %>%
+      summarize(min_edge = min(edge_length)) %>%
+      pull(min_edge)
+  }
+}
+
+minimum <- ATSP(min_dist_matrix, labels = lib_names)
+
+best_tour <- NULL
+best_time <- Inf
+
+for (i in 1:1000) {
+  current_tour <- solve_TSP(minimum)
+  current_time <- tour_length(current_tour)
+  if (current_time < best_time) {
+    best_time <- current_time
+    best_tour <- current_tour
+  }
+}
+
+# Lower bound of about 10.5 hours
+
+# Lower Bound (w/ Cycling) ------------------------------------------------
+
+cycling_data <- tibble()
+
+for (hour in 6:16) {
+  file_name = paste0("otp_cycling_", hour, ".RData")
+  file_path <- here("data", file_name)
+  load(file_path)
+  cycling_data <- rbind(cycling_data, otp_edges)
+  rm(otp_edges)
+}
+
+min_cycling_matrix <- matrix(NA, 17, 17)
+
+for (i in 1:17) {
+  for (j in 1:17) {
+    min_cycling_matrix[i, j] <- cycling_data %>%
+      filter(from == lib_names[i],
+             to == lib_names[j]) %>%
+      summarize(min_edge = min(edge_length)) %>%
+      pull(min_edge)
+  }
+}
+
+# min cycling times are higher than min walking times ???
